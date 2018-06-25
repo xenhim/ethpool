cef9643daf8c0ccc4a6fda18166a62a19ea10c6c
a <a>
2018-06-25 21:02:38 +0800

    pass

--- proxy/miner.go
+++ proxy/miner.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"bytes"
 	"testing"
+	"encoding/binary"
 
 	"github.com/sammy007/open-ethereum-pool/cryptonight"
 	"github.com/ethereum/ethash"
@@ -24,6 +25,26 @@ func TestHashBytes(t *testing.T) {
         }
 }
 
+// cryptonight hash function for new proof-of-work, no more mixing is added, modified by ZorroII
+func hashcryptonight(hash []byte, nonce uint64) ([]byte, []byte) {
+
+        //seed := make([]byte, 40)
+        seed := make([]byte, 48) //fixed by toints
+        copy(seed, hash)
+        //binary.LittleEndian.PutUint64(seed[32:], nonce)
+        /*   
+        *     seed in memory:
+        *     hash      | \0x00    | nonce
+        *     ----------|----------|------
+        *     32 bytes  | 8 bytes  | 8 bytes
+        */
+        binary.LittleEndian.PutUint64(seed[40:], nonce) //fixed by toints
+        digest := cryptonight.HashBytes(seed)
+        //ethash, the pow of ethereum is a heavy calculation, ethereum use a mixhash=sha256(headerhash+digest)
+        //to prevent ddos attack. cryptonight is not so heavy and sha256 is not cpu friendly, so we remove this thing.
+        //the mixhash in header is just digest of header+nonce
+        return digest, digest
+}
 
 func (s *ProxyServer) processShare(login, id, ip string, t *BlockTemplate, params []string) (bool, bool) {
 	nonceHex := params[0]
@@ -31,6 +52,8 @@ func (s *ProxyServer) processShare(login, id, ip string, t *BlockTemplate, param
 	mixDigest := params[2]
 	nonce, _ := strconv.ParseUint(strings.Replace(nonceHex, "0x", "", -1), 16, 64)
 	shareDiff := s.config.Proxy.Difficulty
+
+	maxUint256 := new(big.Int).Exp(big.NewInt(2), big.NewInt(256), big.NewInt(0))
 
 	h, ok := t.headers[hashNoNonce]
 	if !ok {
@@ -53,13 +76,34 @@ func (s *ProxyServer) processShare(login, id, ip string, t *BlockTemplate, param
 		nonce:       nonce,
 		mixDigest:   common.HexToHash(mixDigest),
 	}
+	log.Printf("%+v",shareDiff);
+	log.Printf("%#v,", h.diff);
 
+	log.Printf("%+x", share);
+	log.Printf("%#v", share);
+	log.Printf("%+x", block);
+	log.Printf("%#v", block);
 
-	if !hasher.Verify(share) {
+        digest,mix := hashcryptonight(share.hashNoNonce.Bytes(), share.nonce);
+
+	if bytes.Equal(digest, mix) {}
+
+	if !bytes.Equal(block.mixDigest.Bytes(), digest){
+		log.Printf("digest incorrect!");
 		return false, false
 	}
 
-	if hasher.Verify(block) {
+	targetShare := new(big.Int).Div(maxUint256, share.difficulty)
+	if new(big.Int).SetBytes(digest).Cmp(targetShare) <= 0 {
+		log.Printf("share accepted")
+	}else{
+		return false, false
+	}
+	//if !hasher.Verify(share) { return false, false }
+
+	targetBlock := new(big.Int).Div(maxUint256, block.difficulty)
+	if new(big.Int).SetBytes(digest).Cmp(targetBlock) <= 0 {
+	//if hasher.Verify(block) {
 		ok, err := s.rpc().SubmitBlock(params)
 		if err != nil {
 			log.Printf("Block submission failure at height %v for %v: %v", h.height, t.Header, err)
